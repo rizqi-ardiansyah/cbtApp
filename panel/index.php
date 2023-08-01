c6bd75b1225d4d8248e5d76853fa8fc581360afa
Muhammad Rizqi Ardiansyah <muh.rizqiardian@gmail.com>
2023-08-01 14:14:52 +0700

    #firstupload

---
+++ panel/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=pages/index.php">
+<title>CBT SMP NURUL HUDA </title>
+<script language="javascript">
+    window.location.href = "pages/index.php"
+</script>
+</head>
+<body>
+Go to <a href="pages/index.php">Halaman Administrator CBT SMP NURUL HUDA </a>
+</body>
+</html>
